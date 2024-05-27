e8d24f22ac4279a3e4b24a3e792636e7696603a3
crasysky <122861347+crasysky@users.noreply.github.com>
2024-05-27 20:12:29 +0800

    Add files via upload

---
+++ example.cpp
@@ -0,0 +1,15 @@
+#include "diam.h"
+#include <iostream>
+using namespace std;
+
+void init(int n, double eps){
+    return;
+}
+
+void modify(int op, int x, int y){
+    return;
+}
+
+double report(){
+    return 0;
+}
